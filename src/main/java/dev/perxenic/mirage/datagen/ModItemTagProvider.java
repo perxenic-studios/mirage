36ba147b585a4abfed7530644ed3e14f3219cee2
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-01-29 20:51:27 +0000

    Added datagen for tags

--- src/main/java/dev/perxenic/mirage/datagen/ModItemTagProvider.java
+++ src/main/java/dev/perxenic/mirage/datagen/ModItemTagProvider.java
@@ -1,11 +1,13 @@
 package dev.perxenic.mirage.datagen;
 
 import dev.perxenic.mirage.Mirage;
+import dev.perxenic.mirage.content.MirageItemTags;
 import dev.perxenic.mirage.registry.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
@@ -25,5 +27,22 @@ public class ModItemTagProvider extends ItemTagsProvider {
                 .add(ModItems.ARMADILLO_POTTERY_SHERD.get());
         tag(ItemTags.DECORATED_POT_SHERDS)
                 .add(ModItems.ARMADILLO_POTTERY_SHERD.get());
+
+        tag(MirageItemTags.BADLANDS_RUINS_SHERDS).add(
+                Items.SKULL_POTTERY_SHERD,
+                Items.PLENTY_POTTERY_SHERD,
+                ModItems.ARMADILLO_POTTERY_SHERD.get()
+        );
+        tag(MirageItemTags.DESERT_RUINS_SHERDS).add(
+                Items.SKULL_POTTERY_SHERD,
+                Items.DANGER_POTTERY_SHERD,
+                ModItems.ARMADILLO_POTTERY_SHERD.get()
+        );
+        tag(MirageItemTags.PLATEAU_RUINS_SHERDS).add(
+                Items.PRIZE_POTTERY_SHERD,
+                Items.PLENTY_POTTERY_SHERD,
+                Items.MINER_POTTERY_SHERD,
+                ModItems.ARMADILLO_POTTERY_SHERD.get()
+        );
     }
 }
