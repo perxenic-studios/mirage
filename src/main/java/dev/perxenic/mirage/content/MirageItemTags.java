db3b2b8a999d84f55796fbeafc47534fc08d86dc
Auseawesome <ajthomasbtb@outlook.com>
2026-01-30 16:36:04 +0000

    Fixed missing final modifiers

--- src/main/java/dev/perxenic/mirage/content/MirageItemTags.java
+++ src/main/java/dev/perxenic/mirage/content/MirageItemTags.java
@@ -7,7 +7,7 @@ import net.minecraft.world.item.Item;
 import static dev.perxenic.mirage.Mirage.mirageLoc;
 
 public class MirageItemTags {
-    public static TagKey<Item> BADLANDS_RUINS_SHERDS = ItemTags.create(mirageLoc("badlands_ruins_sherds"));
-    public static TagKey<Item> DESERT_RUINS_SHERDS = ItemTags.create(mirageLoc("desert_ruins_sherds"));
-    public static TagKey<Item> PLATEAU_RUINS_SHERDS = ItemTags.create(mirageLoc("plateau_ruins_sherds"));
+    public final static TagKey<Item> BADLANDS_RUINS_SHERDS = ItemTags.create(mirageLoc("badlands_ruins_sherds"));
+    public final static TagKey<Item> DESERT_RUINS_SHERDS = ItemTags.create(mirageLoc("desert_ruins_sherds"));
+    public final static TagKey<Item> PLATEAU_RUINS_SHERDS = ItemTags.create(mirageLoc("plateau_ruins_sherds"));
 }
