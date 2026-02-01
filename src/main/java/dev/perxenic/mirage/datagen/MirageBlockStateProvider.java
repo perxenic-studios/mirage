06a9c69518a6d9d1211b4aed68e67d8c1a2e4eb6
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-02-01 13:24:29 +0000

    Switched to using list and helper

--- src/main/java/dev/perxenic/mirage/datagen/MirageBlockStateProvider.java
+++ src/main/java/dev/perxenic/mirage/datagen/MirageBlockStateProvider.java
@@ -1,6 +1,7 @@
 package dev.perxenic.mirage.datagen;
 
 import dev.perxenic.mirage.registry.MirageBlocks;
+import dev.perxenic.mirage.util.MirageBlockStateHelper;
 import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.level.block.Block;
@@ -21,51 +22,14 @@ public class MirageBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        fadedTerracotta(MirageBlocks.FADED_SUN_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_MODERN_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_CROSS_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_CRAWL_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_TARGET_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_POTION_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_FISH_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_FLOWER_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_SPOKE_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_PLANT_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_CREEP_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_GEO_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_FAN_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_BLADE_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_ARROW_TERRACOTTA);
-        fadedTerracotta(MirageBlocks.FADED_LEAF_TERRACOTTA);
+        for (DeferredBlock<GlazedTerracottaBlock> block : MirageBlocks.FADED_TERRACOTTA) {
+            MirageBlockStateHelper.fadedTerracotta(this, block);
+        }
 
-        brushableBlock(MirageBlocks.SUSPICIOUS_RED_SAND.get(), "suspicious_red_sand");
-    }
-
-    public void brushableBlock(Block block, String name) {
-        getVariantBuilder(block).forAllStates(state -> {
-            int dusted = state.getValue(BlockStateProperties.DUSTED);
-
-            return ConfiguredModel.builder().modelFile(models().cubeAll(
-                    name + "_" + dusted,
-                    mirageLoc("block/" + name + "_" + dusted)
-            )).build();
-        });
-        simpleBlockItem(block, models().getExistingFile(mirageLoc(name + "_0")));
-    }
-
-    public void fadedTerracotta(DeferredBlock<GlazedTerracottaBlock> block) {
-        String name = block.getId().getPath();
-
-        getVariantBuilder(block.get()).forAllStates(state -> {
-            Direction facing = state.getValue(BlockStateProperties.HORIZONTAL_FACING);
-
-            return ConfiguredModel.builder().modelFile(models().singleTexture(
-                    name,
-                    mcLoc("block/template_glazed_terracotta"),
-                    "pattern",
-                    mirageLoc("block/" + name)
-            )).rotationY((int) facing.toYRot()).build();
-        });
-        simpleBlockItem(block.get(), models().getExistingFile(mirageLoc(name)));
+        MirageBlockStateHelper.brushableBlock(
+                this,
+                MirageBlocks.SUSPICIOUS_RED_SAND.get(),
+                "suspicious_red_sand"
+        );
     }
 }
