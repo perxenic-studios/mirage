e2c2214ff71e7792380cbc06caa9965d3ac5897b
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-03-21 21:44:06 +0000

    Update recipe provider

--- src/main/java/dev/perxenic/mirage/datagen/MirageDataGenerators.java
+++ src/main/java/dev/perxenic/mirage/datagen/MirageDataGenerators.java
@@ -5,8 +5,6 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.BlockTagsProvider;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 import java.util.concurrent.CompletableFuture;
@@ -22,7 +20,7 @@ public class MirageDataGenerators {
         generator.addProvider(true, new MirageBlockTagProvider(packOutput, lookupProvider));
         generator.addProvider(true, new MirageItemTagProvider(packOutput, lookupProvider));
 
-        // generator.addProvider(true, new MirageRecipeProvider.Runner(packOutput, lookupProvider));
+        generator.addProvider(true, new MirageRecipeProvider.Runner(packOutput, lookupProvider));
 
         generator.addProvider(true, new MirageModelProvider(packOutput));
     }
--- src/main/java/dev/perxenic/mirage/datagen/MirageRecipeProvider.java
+++ src/main/java/dev/perxenic/mirage/datagen/MirageRecipeProvider.java
@@ -1,23 +1,25 @@
 package dev.perxenic.mirage.datagen;
 
+import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
 import dev.perxenic.mirage.content.conditions.MirageConfigCondition;
 import dev.perxenic.mirage.registry.MirageBlocks;
 import dev.perxenic.mirage.registry.MirageItems;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.CookingBookCategory;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
-import net.neoforged.neoforge.common.conditions.IConditionBuilder;
-import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.concurrent.CompletableFuture;
 
-import static dev.perxenic.mirage.Mirage.mirageLoc;
-
-public class MirageRecipeProvider extends RecipeProvider implements IConditionBuilder {
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public class MirageRecipeProvider extends RecipeProvider {
 
     public static final MirageConfigCondition fadedTerracottaSmeltingCondition =
             new MirageConfigCondition("fadedTerracottaSmelting");
@@ -28,159 +30,160 @@ public class MirageRecipeProvider extends RecipeProvider implements IConditionBu
     public static final MirageConfigCondition sherdConstructingCondition =
             new MirageConfigCondition("sherdConstructing");
 
-    public MirageRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
-        super(output, registries);
+    public MirageRecipeProvider(HolderLookup.Provider provider, RecipeOutput recipeOutput) {
+        super(provider, recipeOutput);
     }
 
     @Override
-    protected void buildRecipes(@NotNull RecipeOutput recipeOutput) {
-        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, MirageItems.ARMADILLO_BASKET)
+    protected void buildRecipes() {
+        shaped(RecipeCategory.TOOLS, MirageItems.ARMADILLO_BASKET)
                 .pattern(" S ")
                 .pattern("A A")
                 .pattern("AAA")
                 .define('S', Items.STRING)
                 .define('A', Items.ARMADILLO_SCUTE)
                 .unlockedBy("has_armadillo_scute", has(Items.ARMADILLO_SCUTE))
-                .save(recipeOutput, mirageLoc("armadillo_basket"));
+                .save(output, "armadillo_basket");
 
-        ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, MirageItems.ARMADILLO_CHESTPLATE)
+        shaped(RecipeCategory.COMBAT, MirageItems.ARMADILLO_CHESTPLATE)
                 .pattern("A A")
                 .pattern("AAA")
                 .pattern("AAA")
                 .define('A', Items.ARMADILLO_SCUTE)
                 .unlockedBy("has_armadillo_scute", has(Items.ARMADILLO_SCUTE))
-                .save(recipeOutput, mirageLoc("armadillo_chestplate"));
+                .save(output, "armadillo_chestplate");
 
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.WHITE_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_SUN_TERRACOTTA,
                 "faded_sun_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.LIGHT_GRAY_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_MODERN_TERRACOTTA,
                 "faded_modern_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.GRAY_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_CROSS_TERRACOTTA,
                 "faded_cross_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.BLACK_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_CRAWL_TERRACOTTA,
                 "faded_crawl_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.BROWN_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_TARGET_TERRACOTTA,
                 "faded_target_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.RED_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_POTION_TERRACOTTA,
                 "faded_potion_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.ORANGE_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_FISH_TERRACOTTA,
                 "faded_fish_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.YELLOW_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_FLOWER_TERRACOTTA,
                 "faded_flower_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.LIME_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_SPOKE_TERRACOTTA,
                 "faded_spoke_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.GREEN_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_PLANT_TERRACOTTA,
                 "faded_plant_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.CYAN_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_CREEP_TERRACOTTA,
                 "faded_creep_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.LIGHT_BLUE_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_GEO_TERRACOTTA,
                 "faded_geo_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.BLUE_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_FAN_TERRACOTTA,
                 "faded_fan_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.PURPLE_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_BLADE_TERRACOTTA,
                 "faded_blade_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.MAGENTA_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_ARROW_TERRACOTTA,
                 "faded_arrow_terracotta"
         );
-        fadedTerracottaSmelting(recipeOutput,
+        fadedTerracottaSmelting(
                 Items.PINK_GLAZED_TERRACOTTA,
                 MirageBlocks.FADED_LEAF_TERRACOTTA,
                 "faded_leaf_terracotta"
         );
 
         SimpleCookingRecipeBuilder.smelting(
-                        Ingredient.of(ItemTags.DECORATED_POT_SHERDS),
+                        Ingredient.of(BuiltInRegistries.ITEM.getOrThrow(ItemTags.DECORATED_POT_SHERDS)),
                         RecipeCategory.MISC,
+                        CookingBookCategory.MISC,
                         MirageItems.CRACKED_POTTERY_SHERD,
                         0f,
                         200)
                 .unlockedBy("has_sherd", has(ItemTags.DECORATED_POT_SHERDS))
                 .save(
-                        recipeOutput.withConditions(new MirageConfigCondition("sherdCracking")),
-                        mirageLoc("sherd_cracking")
+                        output.withConditions(new MirageConfigCondition("sherdCracking")),
+                        "sherd_cracking"
                 );
 
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, MirageItems.BLANK_POTTERY_SHERD)
+        shapeless(RecipeCategory.MISC, MirageItems.BLANK_POTTERY_SHERD)
                 .requires(MirageItems.CRACKED_POTTERY_SHERD)
                 .requires(Items.BRICK)
                 .group("pottery_sherd")
                 .unlockedBy("has_cracked_sherd", has(MirageItems.CRACKED_POTTERY_SHERD))
                 .save(
-                        recipeOutput.withConditions(new MirageConfigCondition("sherdRepairing")),
-                        mirageLoc("sherd_repairing")
+                        output.withConditions(new MirageConfigCondition("sherdRepairing")),
+                        "sherd_repairing"
                 );
 
-        sherdCrafting(recipeOutput, Items.FISHING_ROD, Items.ANGLER_POTTERY_SHERD, "angler");
-        sherdCrafting(recipeOutput, Items.BOW, Items.ARCHER_POTTERY_SHERD, "archer");
-        sherdCrafting(recipeOutput, Items.RABBIT_HIDE, Items.ARMS_UP_POTTERY_SHERD, "arms_up");
-        sherdCrafting(recipeOutput, Items.STONE_SWORD, Items.BLADE_POTTERY_SHERD, "blade");
-        sherdCrafting(recipeOutput, Items.GLASS_BOTTLE, Items.BREWER_POTTERY_SHERD, "brewer");
-        sherdCrafting(recipeOutput, Items.CAMPFIRE, Items.BURN_POTTERY_SHERD, "burn");
-        sherdCrafting(recipeOutput, Items.GUNPOWDER, Items.DANGER_POTTERY_SHERD, "danger");
-        sherdCrafting(recipeOutput, Items.CHISELED_TUFF, Items.FLOW_POTTERY_SHERD, "flow");
-        sherdCrafting(recipeOutput, Items.MAP, Items.EXPLORER_POTTERY_SHERD, "explorer");
-        sherdCrafting(recipeOutput, Items.EMERALD, Items.FRIEND_POTTERY_SHERD, "friend");
-        sherdCrafting(recipeOutput, Items.WIND_CHARGE, Items.GUSTER_POTTERY_SHERD, "guster");
-        sherdCrafting(recipeOutput, Items.GOLDEN_APPLE, Items.HEART_POTTERY_SHERD, "heart");
-        sherdCrafting(recipeOutput, Items.POISONOUS_POTATO, Items.HEARTBREAK_POTTERY_SHERD, "heartbreak");
-        sherdCrafting(recipeOutput, Items.BONE, Items.HOWL_POTTERY_SHERD, "howl");
-        sherdCrafting(recipeOutput, Items.STONE_PICKAXE, Items.MINER_POTTERY_SHERD, "miner");
-        sherdCrafting(recipeOutput, Items.SCULK , Items.MOURNER_POTTERY_SHERD, "mourner");
-        sherdCrafting(recipeOutput, Items.CHEST, Items.PLENTY_POTTERY_SHERD, "plenty");
-        sherdCrafting(recipeOutput, Items.DIAMOND, Items.PRIZE_POTTERY_SHERD, "prize");
-        sherdCrafting(recipeOutput, Items.STONE_AXE, Items.SCRAPE_POTTERY_SHERD, "scrape");
-        sherdCrafting(recipeOutput, Items.WHEAT, Items.SHEAF_POTTERY_SHERD, "sheaf");
-        sherdCrafting(recipeOutput, Items.ACACIA_SAPLING, Items.SHELTER_POTTERY_SHERD, "shelter");
-        sherdCrafting(recipeOutput, Items.WITHER_SKELETON_SKULL, Items.SKULL_POTTERY_SHERD, "skull");
-        sherdCrafting(recipeOutput, Items.PITCHER_POD, Items.SNORT_POTTERY_SHERD, "snort");
-        sherdCrafting(recipeOutput, Items.ARMADILLO_SCUTE, MirageItems.HIDE_POTTERY_SHERD, "hide");
-        sherdCrafting(recipeOutput, Items.CACTUS, MirageItems.BARREN_POTTERY_SHERD, "barren");
+        sherdCrafting(Items.FISHING_ROD, Items.ANGLER_POTTERY_SHERD, "angler");
+        sherdCrafting(Items.BOW, Items.ARCHER_POTTERY_SHERD, "archer");
+        sherdCrafting(Items.RABBIT_HIDE, Items.ARMS_UP_POTTERY_SHERD, "arms_up");
+        sherdCrafting(Items.STONE_SWORD, Items.BLADE_POTTERY_SHERD, "blade");
+        sherdCrafting(Items.GLASS_BOTTLE, Items.BREWER_POTTERY_SHERD, "brewer");
+        sherdCrafting(Items.CAMPFIRE, Items.BURN_POTTERY_SHERD, "burn");
+        sherdCrafting(Items.GUNPOWDER, Items.DANGER_POTTERY_SHERD, "danger");
+        sherdCrafting(Items.CHISELED_TUFF, Items.FLOW_POTTERY_SHERD, "flow");
+        sherdCrafting(Items.MAP, Items.EXPLORER_POTTERY_SHERD, "explorer");
+        sherdCrafting(Items.EMERALD, Items.FRIEND_POTTERY_SHERD, "friend");
+        sherdCrafting(Items.WIND_CHARGE, Items.GUSTER_POTTERY_SHERD, "guster");
+        sherdCrafting(Items.GOLDEN_APPLE, Items.HEART_POTTERY_SHERD, "heart");
+        sherdCrafting(Items.POISONOUS_POTATO, Items.HEARTBREAK_POTTERY_SHERD, "heartbreak");
+        sherdCrafting(Items.BONE, Items.HOWL_POTTERY_SHERD, "howl");
+        sherdCrafting(Items.STONE_PICKAXE, Items.MINER_POTTERY_SHERD, "miner");
+        sherdCrafting(Items.SCULK, Items.MOURNER_POTTERY_SHERD, "mourner");
+        sherdCrafting(Items.CHEST, Items.PLENTY_POTTERY_SHERD, "plenty");
+        sherdCrafting(Items.DIAMOND, Items.PRIZE_POTTERY_SHERD, "prize");
+        sherdCrafting(Items.STONE_AXE, Items.SCRAPE_POTTERY_SHERD, "scrape");
+        sherdCrafting(Items.WHEAT, Items.SHEAF_POTTERY_SHERD, "sheaf");
+        sherdCrafting(Items.ACACIA_SAPLING, Items.SHELTER_POTTERY_SHERD, "shelter");
+        sherdCrafting(Items.WITHER_SKELETON_SKULL, Items.SKULL_POTTERY_SHERD, "skull");
+        sherdCrafting(Items.PITCHER_POD, Items.SNORT_POTTERY_SHERD, "snort");
+        sherdCrafting(Items.ARMADILLO_SCUTE, MirageItems.HIDE_POTTERY_SHERD, "hide");
+        sherdCrafting(Items.CACTUS, MirageItems.BARREN_POTTERY_SHERD, "barren");
 
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, MirageItems.BLANK_POTTERY_SHERD)
+        shaped(RecipeCategory.MISC, MirageItems.BLANK_POTTERY_SHERD)
                 .pattern("#")
                 .pattern(".")
                 .pattern("#")
@@ -189,43 +192,61 @@ public class MirageRecipeProvider extends RecipeProvider implements IConditionBu
                 .group("pottery_sherd")
                 .unlockedBy("has_brick", has(Items.BRICK))
                 .save(
-                        recipeOutput.withConditions(new MirageConfigCondition("blankSherdConstructing")),
-                        mirageLoc("blank_sherd_constructing")
+                        output.withConditions(new MirageConfigCondition("blankSherdConstructing")),
+                        "blank_sherd_constructing"
                 );
     }
 
-    public void fadedTerracottaSmelting(RecipeOutput recipeOutput, ItemLike input, ItemLike output, String name) {
+    public void fadedTerracottaSmelting(ItemLike inputItem, ItemLike outputItem, String name) {
         SimpleCookingRecipeBuilder.smelting(
-                        Ingredient.of(input),
+                        Ingredient.of(inputItem),
                         RecipeCategory.BUILDING_BLOCKS,
-                        output,
+                        CookingBookCategory.BLOCKS,
+                        outputItem,
                         0.1f,
                         200)
-                .unlockedBy("has_glazed_terracotta", has(input))
-                .save(recipeOutput.withConditions(fadedTerracottaSmeltingCondition), mirageLoc(name + "_smelting"));
+                .unlockedBy("has_glazed_terracotta", has(inputItem))
+                .save(output.withConditions(fadedTerracottaSmeltingCondition), name + "_smelting");
     }
 
-    public void sherdCrafting(RecipeOutput recipeOutput, ItemLike input, ItemLike output, String name) {
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, output)
+    public void sherdCrafting(ItemLike inputItem, ItemLike outputItem, String name) {
+        shapeless(RecipeCategory.MISC, outputItem)
                 .requires(MirageItems.BLANK_POTTERY_SHERD)
-                .requires(input)
+                .requires(inputItem)
                 .group("pottery_sherd")
                 .unlockedBy("has_blank_sherd", has(MirageItems.BLANK_POTTERY_SHERD))
                 .save(
-                        recipeOutput.withConditions(sherdCraftingCondition),
-                        mirageLoc(name+"_sherd_crafting")
+                        output.withConditions(sherdCraftingCondition),
+                        name + "_sherd_crafting"
                 );
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, output)
+        shaped(RecipeCategory.MISC, outputItem)
                 .pattern("#")
                 .pattern(".")
                 .pattern("#")
                 .define('#', Items.BRICK)
-                .define('.', input)
+                .define('.', inputItem)
                 .group("pottery_sherd")
                 .unlockedBy("has_brick", has(Items.BRICK))
                 .save(
-                        recipeOutput.withConditions(sherdConstructingCondition),
-                        mirageLoc(name+"_sherd_constructing")
+                        output.withConditions(sherdConstructingCondition),
+                        name + "_sherd_constructing"
                 );
+    }
+
+    public static class Runner extends RecipeProvider.Runner {
+
+        public Runner(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+            super(output, registries);
+        }
+
+        @Override
+        protected RecipeProvider createRecipeProvider(HolderLookup.Provider registries, RecipeOutput output) {
+            return new MirageRecipeProvider(registries, output);
+        }
+
+        @Override
+        public String getName() {
+            return "Mirage Recipes";
+        }
     }
 }
