dd0ec732559a8255d6ec9cbe64c6df485ffc1017
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-08-08 20:53:33 +0100

    Desert underground add feature holders

--- src/main/java/dev/perxenic/mirage/datagen/desert_underground/DesertUndergroundBiomeModifers.java
+++ src/main/java/dev/perxenic/mirage/datagen/desert_underground/DesertUndergroundBiomeModifers.java
@@ -7,34 +7,38 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.neoforged.neoforge.common.world.BiomeModifier;
+
+import java.util.List;
+
 import static dev.perxenic.mirage.datagen.helpers.BiomeModifierHelper.addFeatureBiomeModifer;
 
 public class DesertUndergroundBiomeModifers {
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         HolderGetter<Biome> biomes = context.lookup(Registries.BIOME);
-        HolderGetter<PlacedFeature> placedFeatures = context.lookup(Registries.PLACED_FEATURE);
 
         addFeatureBiomeModifer(
-                context, biomes, placedFeatures,
+                context, biomes,
                 GenerationStep.Decoration.UNDERGROUND_ORES,
                 "add_sandy_stone",
                 "has_sandy_stone",
-                "placed_sandy_stone"
+                List.of(DesertUndergroundPlacedFeatures.SANDY_STONE)
         );
         addFeatureBiomeModifer(
-                context, biomes, placedFeatures,
+                context, biomes,
                 GenerationStep.Decoration.UNDERGROUND_DECORATION,
                 "add_pointed_sandstone",
                 "has_sandy_stone",
-                "placed_pointed_sandstone",
-                "placed_floor_pointed_sandstone"
+                List.of(
+                        DesertUndergroundPlacedFeatures.POINTED_SANDSTONE,
+                        DesertUndergroundPlacedFeatures.FLOOR_POINTED_SANDSTONE
+                )
         );
         addFeatureBiomeModifer(
-                context, biomes, placedFeatures,
+                context, biomes,
                 GenerationStep.Decoration.UNDERGROUND_DECORATION,
                 "add_underground_dry_grass",
                 "has_sandy_stone",
-                "placed_underground_dry_grass"
+                List.of(DesertUndergroundPlacedFeatures.UNDERGROUND_DRY_GRASS)
         );
     }
 }
