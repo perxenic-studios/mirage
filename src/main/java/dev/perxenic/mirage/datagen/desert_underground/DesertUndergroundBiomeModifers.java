b5949a57c801cb6516f156cdefba2cba3b0719aa
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-08-08 17:03:21 +0100

    Moved helper method for biome modifier to class

--- src/main/java/dev/perxenic/mirage/datagen/desert_underground/DesertUndergroundBiomeModifers.java
+++ src/main/java/dev/perxenic/mirage/datagen/desert_underground/DesertUndergroundBiomeModifers.java
@@ -1,21 +1,13 @@
 package dev.perxenic.mirage.datagen.desert_underground;
 
 import net.minecraft.core.HolderGetter;
-import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.neoforged.neoforge.common.world.BiomeModifier;
-import net.neoforged.neoforge.common.world.BiomeModifiers;
-import net.neoforged.neoforge.registries.NeoForgeRegistries;
-
-import java.util.Arrays;
-
-import static dev.perxenic.mirage.Mirage.mirageLoc;
+import static dev.perxenic.mirage.datagen.helpers.BiomeModifierHelper.addFeatureBiomeModifer;
 
 public class DesertUndergroundBiomeModifers {
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
@@ -43,29 +35,6 @@ public class DesertUndergroundBiomeModifers {
                 "add_underground_dry_grass",
                 "has_sandy_stone",
                 "placed_underground_dry_grass"
-        );
-    }
-
-    private static void addFeatureBiomeModifer(
-            BootstrapContext<BiomeModifier> context,
-            HolderGetter<Biome> biomes,
-            HolderGetter<PlacedFeature> placedFeatureLookup,
-            GenerationStep.Decoration generationStep,
-            String name,
-            String biomeTag,
-            String... placedFeatures
-    ) {
-        var features = Arrays.stream(placedFeatures).map(feature ->
-                placedFeatureLookup.getOrThrow(ResourceKey.create(Registries.PLACED_FEATURE, mirageLoc(feature)))
-        ).toList();
-
-        context.register(
-                ResourceKey.create(NeoForgeRegistries.Keys.BIOME_MODIFIERS, mirageLoc(name)),
-                new BiomeModifiers.AddFeaturesBiomeModifier(
-                        biomes.getOrThrow(TagKey.create(Registries.BIOME, mirageLoc(biomeTag))),
-                        HolderSet.direct(features),
-                        generationStep
-                )
         );
     }
 }
---
+++ src/main/java/dev/perxenic/mirage/datagen/helpers/BiomeModifierHelper.java
@@ -0,0 +1,43 @@
+package dev.perxenic.mirage.datagen.helpers;
+
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.common.world.BiomeModifiers;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+import java.util.Arrays;
+
+import static dev.perxenic.mirage.Mirage.mirageLoc;
+
+public class BiomeModifierHelper {
+    public static void addFeatureBiomeModifer(
+            BootstrapContext<BiomeModifier> context,
+            HolderGetter<Biome> biomes,
+            HolderGetter<PlacedFeature> placedFeatureLookup,
+            GenerationStep.Decoration generationStep,
+            String name,
+            String biomeTag,
+            String... placedFeatures
+    ) {
+        var features = Arrays.stream(placedFeatures).map(feature ->
+                placedFeatureLookup.getOrThrow(ResourceKey.create(Registries.PLACED_FEATURE, mirageLoc(feature)))
+        ).toList();
+
+        context.register(
+                ResourceKey.create(NeoForgeRegistries.Keys.BIOME_MODIFIERS, mirageLoc(name)),
+                new BiomeModifiers.AddFeaturesBiomeModifier(
+                        biomes.getOrThrow(TagKey.create(Registries.BIOME, mirageLoc(biomeTag))),
+                        HolderSet.direct(features),
+                        generationStep
+                )
+        );
+    }
+}
