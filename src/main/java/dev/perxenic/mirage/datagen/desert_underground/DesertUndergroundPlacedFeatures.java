e6776decf51f7cdf9a240951b531f2da1f6d7179
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-08-08 20:43:30 +0100

    Moved dry grass to datagen

--- src/main/java/dev/perxenic/mirage/datagen/desert_underground/DesertUndergroundPlacedFeatures.java
+++ src/main/java/dev/perxenic/mirage/datagen/desert_underground/DesertUndergroundPlacedFeatures.java
@@ -3,12 +3,12 @@ package dev.perxenic.mirage.datagen.desert_underground;
 import dev.perxenic.mirage.registry.MirageBlocks;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
-import net.minecraft.core.Vec3i;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.features.VegetationFeatures;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.valueproviders.TrapezoidInt;
 import net.minecraft.util.valueproviders.UniformInt;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
@@ -25,7 +25,11 @@ public class DesertUndergroundPlacedFeatures {
     static Holder<PlacedFeature> POINTED_SANDSTONE;
     static Holder<PlacedFeature> FLOOR_POINTED_SANDSTONE;
 
+    static Holder<PlacedFeature> UNDERGROUND_DRY_GRASS;
+
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
+        var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+
         SANDY_STONE = context.register(
                 ResourceKey.create(Registries.PLACED_FEATURE, mirageLoc("placed_sandy_stone")),
                 new PlacedFeature(
@@ -57,6 +61,36 @@ public class DesertUndergroundPlacedFeatures {
                 pointedSandstoneFeature(
                         DesertUndergroundConfiguredFeatures.FLOOR_POINTED_SANDSTONE,
                         Direction.DOWN
+                )
+        );
+
+        UNDERGROUND_DRY_GRASS = context.register(
+                ResourceKey.create(Registries.PLACED_FEATURE, mirageLoc("placed_underground_dry_grass")),
+                new PlacedFeature(
+                        configuredFeatures.getOrThrow(VegetationFeatures.DRY_GRASS),
+                        List.of(
+                                CountPlacement.of(UniformInt.of(1024, 2048)),
+                                InSquarePlacement.spread(),
+                                HeightRangePlacement.of(
+                                        TrapezoidHeight.of(
+                                                VerticalAnchor.absolute(24),
+                                                VerticalAnchor.absolute(256)
+                                        )
+                                ),
+                                EnvironmentScanPlacement.scanningFor(
+                                        Direction.DOWN,
+                                        BlockPredicate.matchesBlocks(MirageBlocks.SANDY_STONE.get()),
+                                        BlockPredicate.ONLY_IN_AIR_PREDICATE,
+                                        12
+                                ),
+                                BiomeFilter.biome(),
+                                CountPlacement.of(64),
+                                RandomOffsetPlacement.of(
+                                        TrapezoidInt.triangle(7),
+                                        TrapezoidInt.triangle(3)
+                                ),
+                                BlockPredicateFilter.forPredicate(BlockPredicate.ONLY_IN_AIR_PREDICATE)
+                        )
                 )
         );
     }
