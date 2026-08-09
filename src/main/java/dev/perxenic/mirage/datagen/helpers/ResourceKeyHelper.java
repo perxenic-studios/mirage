a589270e7ab1d93dd07a31e783fa758ac9e49e16
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-08-09 19:15:27 +0100

    Add configured feature to resource key helper

--- src/main/java/dev/perxenic/mirage/datagen/helpers/ResourceKeyHelper.java
+++ src/main/java/dev/perxenic/mirage/datagen/helpers/ResourceKeyHelper.java
@@ -2,11 +2,15 @@ package dev.perxenic.mirage.datagen.helpers;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 import static dev.perxenic.mirage.Mirage.mirageLoc;
 
 public class ResourceKeyHelper {
+    public static ResourceKey<ConfiguredFeature<?, ?>> mirConfiguredFeature(String id) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, mirageLoc(id));
+    }
     public static ResourceKey<PlacedFeature> mirPlacedFeature(String id) {
         return ResourceKey.create(Registries.PLACED_FEATURE, mirageLoc(id));
     }
